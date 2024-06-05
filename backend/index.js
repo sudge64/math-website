d6bd73cfa40596368464669c168d1f4d734ee834
C.J. Wade <sudge64@icloud.com>
2024-06-05 15:34:21 -0500

    Get backend & frontend to talk to each other

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const port = 3001;
+
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+app.get('/', (req, res) => {
+  res.send('Hello from backend');
 });
 
-app.listen(3001);
+app.listen(port, () => {
+  console.log(`Running on ${port}`);
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:3001/')
+      .then(response => response.text())
+      .then(data => setMessage(data))
+  })
 
   return (
     <>
@@ -28,6 +36,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>{message}</p>
     </>
   )
 }

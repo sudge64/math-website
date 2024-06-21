7cea34ac108ec831b9af0f994da0db550df0ac2e
C.J. Wade <sudge64@icloud.com>
2024-06-20 20:27:00 -0500

    Add rust file for basic math functions, Warning: no checking for zero on / or % yet

--- number-cruncher/src/main.rs
+++ number-cruncher/src/main.rs
@@ -1,3 +1,11 @@
+mod math_basic;
+
 fn main() {
-    println!("Hello, world!");
+    let a = 1.0;
+    let b = 0.0;
+    println!("Addition: {}", math_basic::addition(a, b));
+    println!("Subtraction: {}", math_basic::subtraction(a, b));
+    println!("Multiplication: {}", math_basic::multiplication(a, b));
+    println!("Divison: {}", math_basic::division(a, b));
+    println!("Modulo: {}", math_basic::modulo(a, b));
 }
---
+++ number-cruncher/src/math_basic.rs
@@ -0,0 +1,18 @@
+use core::panic;
+use std::error::Error;
+
+pub fn addition(a: f32, b: f32) -> f32 {
+    a + b
+}
+pub fn subtraction(a: f32, b: f32) -> f32 {
+    a - b
+}
+pub fn multiplication(a: f32, b: f32) -> f32 {
+    a * b
+}
+pub fn division(a: f32, b: f32) -> f32{
+            a / b
+}
+pub fn modulo(a: f32, b: f32) -> f32{
+            a % b
+}

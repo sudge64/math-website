3af3d35b98682020092bfeadbbc1f31d02eb5e92
C.J. Wade <sudge64@icloud.com>
2024-08-12 17:25:55 -0500

    Fix: Add description

--- number-cruncher/src/evalutate_rpn.rs
+++ number-cruncher/src/evalutate_rpn.rs
@@ -1,13 +1,20 @@
 /* 
 * Original Author: C.J. Wade
 * License: T.B.D.
-* Description: 
+* Description: Evalutates RPN stacks.
+*               I hacked away at this on my own, but recieved help from
+*               this article: https://medium.com/nplan/aoc-2020-day-18-shunting-yard-algorithm-with-rust-6439cb02dbad
+*               I would like to point out that the only difference between
+*               his solution and mine was me only having one set of `a` & `b`
+*               before the `match` statement. This was causing the complier
+*               to panic when it read a `None` from the empty stack.
 */
 
 pub fn evaluate_rpn(output_queue: Vec<char>) -> u32 {
     let mut stack: Vec<u32> = Vec::new();
 
     for token in output_queue.into_iter() {
+        // My a & b were here.
         match token {
             '+' => {
                 let a: u32 = stack.pop().unwrap();

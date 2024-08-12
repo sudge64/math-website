2bf9147383261cd5f2ec9b7df056ef9830919990
C.J. Wade <sudge64@icloud.com>
2024-08-12 17:28:16 -0500

    Fix: Added `#[cfg(test)]`

--- number-cruncher/src/test.rs
+++ number-cruncher/src/test.rs
@@ -1,6 +1,8 @@
 use crate::shunting_yard::shunting_yard;
 use crate::evaluate_rpn;
 
+#[cfg(test)]
+
 #[test]
 fn shunting_yard_success() {
     let answer: Vec<char> = vec!['3', '4', '2', '*', '1', '5', '-', '2', '3', '^', '^', '/', '+'];

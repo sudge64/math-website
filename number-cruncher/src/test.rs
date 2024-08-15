3c1b8799ee3cee5c1c6d01fff51e9a4e1d547302
C.J. Wade <sudge64@icloud.com>
2024-08-15 12:10:12 -0500

    Fix: Remove unnecessary `#[cfg(test)]`

--- number-cruncher/src/test.rs
+++ number-cruncher/src/test.rs
@@ -1,8 +1,6 @@
 use crate::shunting_yard::shunting_yard;
 use crate::evaluate_rpn;
 
-#[cfg(test)]
-
 #[test]
 fn shunting_yard_success() {
     let answer: Vec<String> = vec!["3".to_string(), "4".to_string(), "2".to_string(), "*".to_string(), "1".to_string(), "5".to_string(), "-".to_string(), "2".to_string(), "3".to_string(), "^".to_string(), "^".to_string(), "/".to_string(), "+".to_string()];
